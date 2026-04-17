692722dcf707b88c099f58c73672c0d224e92512
Victor-DREC <victorpilachanga12@gmail.com>
2026-04-17 16:39:08 -0500

    actualizacion de la funcion calcular

--- simulador.js
+++ simulador.js
@@ -1,4 +1,3 @@
-
 function calcular() {
     let ingresos = parseFloat(document.getElementById('txtIngresos').value) || 0;
     let egresos = parseFloat(document.getElementById('txtEgresos').value) || 0;
@@ -8,6 +7,7 @@ function calcular() {
 
     let capacidadPago = calcularCapacidadPago(disponible);
     document.getElementById('spnCapacidadPago').innerText = capacidadPago.toFixed(2);
+
     let monto = parseInt(document.getElementById('txtMonto').value) || 0;
     let plazo = parseInt(document.getElementById('txtPlazo').value) || 0;
     let tasa = parseInt(document.getElementById('txtTasaInteres').value) || 0;
@@ -19,9 +19,17 @@ function calcular() {
     document.getElementById('spnTotalPrestamo').innerText = totalPrestamo.toFixed(2);
 
     let cuotaMensual = calcularCuotaMensual(totalPrestamo, plazo);
+    document.getElementById('spnCuotaMensual').innerText = cuotaMensual.toFixed(2);
 
-    let componenteCuota = document.getElementById('spnCuotaMensual');
-    if (componenteCuota) {
-        componenteCuota.innerText = cuotaMensual.toFixed(2);
+    let esAprobado = aprobarCredito(capacidadPago, cuotaMensual);
+
+    let lblEstado = document.getElementById('spnEstadoCredito');
+
+    if (esAprobado) {
+        lblEstado.innerText = "CREDITO APROBADO";
+        lblEstado.style.color = "#27ae60"; 
+    } else {
+        lblEstado.innerText = "CREDITO RECHAZADO";
+        lblEstado.style.color = "#e74c3c"; 
     }
 }
