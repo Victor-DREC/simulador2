0df6cf0611a5f2aba3e0a731f0e38be96c195e42
Victor-DREC <victorpilachanga12@gmail.com>
2026-04-17 16:30:32 -0500

    funcion calcularCapacidadPago

--- funciones.js
+++ funciones.js
@@ -1,3 +1,8 @@
 function calcularDisponible(ingresos, egresos) {
     return Math.max(0, ingresos - egresos);
 }
+
+function calcularCapacidadPago(montoDisponible) {
+    let capacidad = montoDisponible * 0.5;
+    return capacidad;
+}
