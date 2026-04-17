246a862dc7fce8cffba89b713d9989af9dc9ee5c
Victor-DREC <victorpilachanga12@gmail.com>
2026-04-17 16:32:42 -0500

    actualizacion de la funcion calcular y adicion de la funcion calcularInteresSimple

--- funciones.js
+++ funciones.js
@@ -6,3 +6,8 @@ function calcularCapacidadPago(montoDisponible) {
     let capacidad = montoDisponible * 0.5;
     return capacidad;
 }
+
+function calcularInteresSimple(monto, tasa, plazoAnios) {
+    let interes = plazoAnios * monto * (tasa / 100);
+    return interes;
+}
--- simulador.js
+++ simulador.js
@@ -1,6 +1,5 @@
 function calcular() {
     let ingresos = parseFloat(document.getElementById('txtIngresos').value);
-    
     let egresos = parseFloat(document.getElementById('txtEgresos').value);
 
     if (isNaN(ingresos)) ingresos = 0;
@@ -8,9 +7,12 @@ function calcular() {
 
     let disponible = calcularDisponible(ingresos, egresos);
 
-    let componenteDisponible = document.getElementById('spnDisponible');
-    
-    if (componenteDisponible) {
-        componenteDisponible.innerText = disponible.toFixed(2);
+    document.getElementById('spnDisponible').innerText = disponible.toFixed(2);
+
+    let capacidadPago = calcularCapacidadPago(disponible);
+
+    let componenteCapacidad = document.getElementById('spnCapacidadPago');
+    if (componenteCapacidad) {
+        componenteCapacidad.innerText = capacidadPago.toFixed(2);
     }
 }
